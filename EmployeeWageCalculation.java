27d38cbdbc1359bf6de815956769cbddbacf76f7
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-06 00:27:29 +0530

    Add: Calculate Wages For A Month

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -3,25 +3,34 @@ package com.assignments.day3.EmployeeWage;
 import java.util.*;
 
 public class EmployeeWageCalculation {
-	static int Wage_per_Hour = 20;
-    static int Full_Day_Hour = 8;
-    static int Part_Time_Hour = 4;
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        Random ran = new Random();
-        int attendance = ran.nextInt(3);
-
-        switch (attendance) {
-            case 1 : {
-                System.out.println("Employee is Present for Full time");
-                System.out.println("Employee`s Daily Wage is => "+Wage_per_Hour * Full_Day_Hour);break;
-            } 
-            case 2 : {
-                System.out.println("Employee is Present for Part time");
-                System.out.println("Employee`s Daily Wage is => "+Wage_per_Hour * Part_Time_Hour);break;
-            } 
-            default : System.out.println("Employee is Absent");break;
-        }
-    }
+	    static int Wage_per_Hour = 20;
+	    static int Full_Day_Hour = 8;
+	    static int Part_Time_Hour = 4;
+	    static int Working_Day_per_Month = 20;
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Employee Wage Computation Program");
+	        int totalWage = 0;
+	        int dayCount = 1;
+	        while (dayCount <= Working_Day_per_Month) {
+	            Random ran = new Random();
+	            int attendance = ran.nextInt(3);
+	            int employeeWage = 0;
+	            switch (attendance) {
+	                case 1 : {
+	                    System.out.println("\nEmployee is Present for Full time.");
+	                    employeeWage = Wage_per_Hour * Full_Day_Hour;break;
+	                }
+	                case 2 : {
+	                    System.out.println("\nEmployee is Present for Part time.");
+	                    employeeWage = Wage_per_Hour * Part_Time_Hour;break;
+	                }
+	                default : System.out.println("\nEmployee is Absent.");break;
+	            }
+	            totalWage += employeeWage;
+	            System.out.println("Employee Wage for day "+dayCount+" is: "+employeeWage);
+	            dayCount++;
+	        }
+	        System.out.println("\nTotal Monthly Wage of Employee is: "+totalWage);
+	    }
   }
 
