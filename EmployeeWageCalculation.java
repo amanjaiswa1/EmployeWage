d354b33d9d5519d20b28e118914afca8e7935c10
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-05 23:59:12 +0530

    Add: Added Part Time Employee & Wage

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -5,16 +5,21 @@ import java.util.*;
 public class EmployeeWageCalculation {
 	static int Wage_per_Hour = 20;
     static int Full_Day_Hour = 8;
+    static int Part_Time_Hour = 4;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random ran = new Random();
-        int attendance = ran.nextInt(2);
+        int attendance = ran.nextInt(3);
 
         if (attendance == 1) {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present for Full time");
             System.out.println("Employee`s Daily Wage is => "+Wage_per_Hour * Full_Day_Hour);
-
-        } else {
+        } 
+        else if (attendance == 2) {
+            System.out.println("Employee is Present for Part time");
+            System.out.println("Employee`s Daily Wage is => "+Wage_per_Hour * Part_Time_Hour);
+        } 
+        else {
             System.out.println("Employee is Absent");
         }
     }
