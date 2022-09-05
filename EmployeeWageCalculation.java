63d5fc9fdbe6ed930c9647dd496549fcea933e6d
amanjaiswa1 <amanjaiswal0608@gmail.com>
2022-09-06 00:46:04 +0530

    Add: Calculate Wages Till Total WOrking Hours Or Days

--- EmployeeWageCalculation.java
+++ EmployeeWageCalculation.java
@@ -7,22 +7,26 @@ public class EmployeeWageCalculation {
 	    static int Full_Day_Hour = 8;
 	    static int Part_Time_Hour = 4;
 	    static int Working_Day_per_Month = 20;
+	    static int Total_Working_Hour = 100;
 	    public static void main(String[] args) {
 	        System.out.println("Welcome to Employee Wage Computation Program");
 	        int totalWage = 0;
 	        int dayCount = 1;
-	        while (dayCount <= Working_Day_per_Month) {
+	        int workingHours = 0;
+	        while (dayCount <= Working_Day_per_Month && workingHours < Total_Working_Hour) {
 	            Random ran = new Random();
 	            int attendance = ran.nextInt(3);
 	            int employeeWage = 0;
 	            switch (attendance) {
 	                case 1 : {
 	                    System.out.println("\nEmployee is Present for Full time.");
-	                    employeeWage = Wage_per_Hour * Full_Day_Hour;break;
+	                    employeeWage = Wage_per_Hour * Full_Day_Hour;
+	                    workingHours += Full_Day_Hour;break;
 	                }
 	                case 2 : {
 	                    System.out.println("\nEmployee is Present for Part time.");
-	                    employeeWage = Wage_per_Hour * Part_Time_Hour;break;
+	                    employeeWage = Wage_per_Hour * Part_Time_Hour;
+	                    workingHours += Part_Time_Hour;break;
 	                }
 	                default : System.out.println("\nEmployee is Absent.");break;
 	            }
@@ -31,6 +35,7 @@ public class EmployeeWageCalculation {
 	            dayCount++;
 	        }
 	        System.out.println("\nTotal Monthly Wage of Employee is: "+totalWage);
+	        System.out.println("Total Working Hours are: "+workingHours);
 	    }
   }
 
